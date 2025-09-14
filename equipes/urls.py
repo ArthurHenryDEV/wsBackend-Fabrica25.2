4d4830fff06f03a46b9a220d65e2149531a4818d
ArthurHenryDEV <arthurdiaspaiva1@gmail.com>
2025-09-14 00:41:57 -0300

    :sparkles:feat:(equipes): CRUD Básico Completo
    - Adicionar funcionalidades de edição e exclusão para instâncias de EquipeModel,
    atualizando as views, URLs e templates correspondentes.
    
    - Melhorar a navegação entre páginas no app 'equipes' e ajusta a exibição das equipes.
    
    -Comentar o campo 'data_criacao' em EquipeModel que não está sendo utilizado,
    preparando o modelo para futuras melhorias.

--- equipes/urls.py
+++ equipes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import busca_equipe, lista_equipe, home_equipe
+from .views import busca_equipe, lista_equipe, home_equipe, deletar_equipe, editar_equipe
 
 urlpatterns = [
 
     path('', home_equipe, name='home_equipe'),
     path('busca/', busca_equipe, name='busca_equipe'),
     path('lista/', lista_equipe, name='lista_equipe'),
+    path('deletar/<int:pk>', deletar_equipe, name='confirmar_deletar_equipe'),
+    path('editar/<int:pk>', editar_equipe, name='editar_equipe'),
 
 ]
--- equipes/views.py
+++ equipes/views.py
@@ -11,12 +11,31 @@ def busca_equipe(request):
         form = EquipeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('cadastro_equipe')
+            return redirect('lista_equipe')
     else: 
         form = EquipeForm()
     return render(request, 'busca_equipe.html', {'form': form})
 
 def lista_equipe(request):
     equipes = EquipeModel.objects.all()
+
+    equipes = EquipeModel.objects.all()
     return render(request, 'lista_equipe.html', {'equipes': equipes})
 
+def deletar_equipe(request, pk):
+    equipe = EquipeModel.objects.get(pk=pk)
+    if request.method == 'POST':
+        equipe.delete()
+        return redirect('lista_equipe')
+    return render(request, 'confirmar_deletar_equipe.html', {'equipe': equipe})
+
+def editar_equipe(request, pk):
+    equipe = EquipeModel.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = EquipeForm(request.POST, instance=equipe)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_equipe')
+    else:
+        form = EquipeForm(instance=equipe)
+    return render(request, 'busca_equipe.html', {'form': form})
